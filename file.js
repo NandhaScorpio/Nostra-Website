01fab00c6bb7463103a0ccfe8ead65d8c136fc3d
NandhaScorpio <gokulamstore2010@gmail.com>
2024-02-08 13:11:12 +0530

    Updating

---
+++ file.js
@@ -0,0 +1,13 @@
+var sidenav = document.querySelector(".side-navbar")
+function openNavbar()
+{
+    sidenav.style.left="0"
+}
+function closeNavbar()
+{
+    sidenav.style.left="-60%"
+}
+function replace()
+{
+    location.href = "collection.html"
+}
